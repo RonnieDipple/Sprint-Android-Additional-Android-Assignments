647813eb8b7c85a38bd37ec66fcc6dbc0af7c7b7
ronniedipple <ronniedipple@gmail.com>
2019-10-07 12:53:02 -0500

    Guided project complete

--- M01/Guided Project/MediaProgramming/app/src/main/java/com/lambdaschool/mediaprogramming/ExoPlayerActivity.kt
+++ M01/Guided Project/MediaProgramming/app/src/main/java/com/lambdaschool/mediaprogramming/ExoPlayerActivity.kt
@@ -21,13 +21,18 @@ This activity demonstrates how to use ExoPlayer to play a video from local file 
 also from the internet.
  */
 
+//TODO this is for PlayerWithURL
+
 //TODO 1: Make sure to turn on the permission for internet in the manifest.
 //TODO 2: Enable 'targetCompatibility JavaVersion.VERSION_1_8' in app build.gradle file.
 //TODO 3: Enable ExoPlayer dependency in the app build.gradle file.
 
+//prepare should not be on MAIN UI THREAD!!!!
+
 class ExoPlayerActivity : AppCompatActivity() {
 
     //TODO 4: Declare the variables needed. A simpleExoPlayer instance.
+   lateinit var videoExoPlayer: SimpleExoPlayer
 
     //TODO 5: Notice the url we will be using to streaming mp4 over the internet.
     val URL = "https://archive.org/download/Popeye_forPresident/Popeye_forPresident_512kb.mp4"
@@ -42,37 +47,57 @@ class ExoPlayerActivity : AppCompatActivity() {
         //TODO 7: Create a function to setup video player from file system
         //This function can be switched out with the setupVideoPlayerWithURL to stream the video
         //from the internet.
-        setupVideoPlayerFromFileSystem()
+       // setupVideoPlayerFromFileSystem()
+
+       setupVideoPlayerWithURL() //TODO this is for PlayerWithURL
 
         //TODO 9: Setup clicklisteners for play and pause buttons
+        play.setOnClickListener { videoExoPlayer.playWhenReady = true }
+        pause.setOnClickListener { videoExoPlayer.playWhenReady = false }
 
         //TODO 9a: Set the player for the PlayerView
+        playerView.player = videoExoPlayer
     }
 
     fun setupVideoPlayerFromFileSystem() {
+        videoExoPlayer.prepare(createRawMediaSource(R.raw.live_views_of_starman))
     }
 
     //TODO 8: Create a function to setup video player with url to stream video through internet.
+
+    //TODO this is for PlayerWithURL
     fun setupVideoPlayerWithURL() {
+        videoExoPlayer.prepare(createUrlMediaSource(URL))
     }
 
     fun createVideoPlayer() {
         // Need a track selector
+        val trackSelector = DefaultTrackSelector()
         // Need a load control
+        val loadControl = DefaultLoadControl()
         // Need a renderers factory
+        val renderersFactory = DefaultRenderersFactory(this)
+
 
         // Set up the ExoPlayer
+        videoExoPlayer = ExoPlayerFactory.newSimpleInstance(this, renderersFactory,
+            trackSelector,
+            loadControl)
 
         // Set up the scaling mode to crop and fit the video to the screen
+        videoExoPlayer.videoScalingMode = C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING //fits player to screen, C is an exo player class
 
     }
 
     //TODO 12: Do not forget to stop the player when the user navigates away from the screen
     override fun onStop() {
         super.onStop()
+        videoExoPlayer.stop()
     }
 
     //TODO 10: Notice the code to implement and create a mediasource function using URL, returns a mediasource
+
+    //TODO this is for PlayerWithURL
     fun createUrlMediaSource(url: String): MediaSource {
         val userAgent = Util.getUserAgent(this, getString(R.string.app_name))
         return ExtractorMediaSource.Factory(DefaultDataSourceFactory(this, userAgent))
@@ -81,6 +106,8 @@ class ExoPlayerActivity : AppCompatActivity() {
     }
 
     //TODO 11: Notice the code to implement and create a mediasource function using raw resource, returns a mediasource
+    //Great boilerplate for exoplayer files on internet
+
     fun createRawMediaSource(@RawRes rawId: Int): MediaSource {
         val rawResourceDataSource = RawResourceDataSource(this)
         val dataSpec = DataSpec(RawResourceDataSource.buildRawResourceUri(rawId))
--- M01/Guided Project/MediaProgramming/app/src/main/java/com/lambdaschool/mediaprogramming/MainActivity.kt
+++ M01/Guided Project/MediaProgramming/app/src/main/java/com/lambdaschool/mediaprogramming/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //TODO 1: Disable the play/pause button by default (you'll enable it later)
+        play_pause_button.isEnabled = false
 
         //TODO 2: Add the play/pause functionality including the animation for the button and
         //TODO playing or pausing the VideoView.
@@ -36,25 +37,67 @@ class MainActivity : AppCompatActivity() {
         // If the video is not playing, start it
         // else pause the video
         // Start the animation
+
+        play_pause_button.setOnClickListener {
+            if(video_view.isPlaying){
+                video_view.pause()
+                play_pause_button.setImageDrawable(getDrawable(R.drawable.avd_anim_pause_play))
+                //pause
+            }else{
+                video_view.start()
+                play_pause_button.setImageDrawable(getDrawable(R.drawable.avd_anim_play_pause))
+
+            }
+
+           val drawable = play_pause_button.drawable
+            if (drawable is Animatable){
+                (drawable as Animatable).start()
+            }
+        }
     }
 
     private fun seekBarFunctionality() {
         // In the SeekBar listener, when the seekbar progress is changed,
         // update the video progress
+        video_seek_bar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                //if seek bar moved seek another location in the video ie move to different section of video
+                seekBar?.let {
+                    video_view.seekTo(it.progress)
+                }
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+
+        })
     }
 
     override fun onStart() {
         super.onStart()
 
         //TODO 4: Set the video URI to the videoview (This video is an mp4 file in the raw folder )
+        //Ignore name not actually it is in the apk
+        video_view.setVideoURI(Uri.parse("android.resource://" + packageName
+                + "/" + R.raw.live_views_of_starman))
 
         //TODO 5: Set an onPreparedListener to the videoview and enable the play/pause button in the
         //TODO callback
+        video_view.setOnPreparedListener { mp ->
+            play_pause_button.isEnabled = true
+            mp?.let {
+                video_seek_bar.max = mp.duration // sets the seek bar end to video end,
+                // duration is an int, it is millisecond scrubbing
+            }
+        }
     }
 
     //TODO 8: Pause the video when onStop is called, if user navigates away from the screen
     override fun onStop() {
         super.onStop()
+        video_view.pause()
     }
 
     //TODO 7: Notice that we create an overflow menu to handle navigation between activities
@@ -65,7 +108,7 @@ class MainActivity : AppCompatActivity() {
 
     //TODO 6: Notice that we will use an overflow menu to navigate to the ExoPlayerActivity
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        startActivity(Intent(this@MainActivity, ExoPlayerActivity::class.java))
+                startActivity(Intent(this@MainActivity, ExoPlayerActivity::class.java))
         return super.onOptionsItemSelected(item)
     }
 
